d5a74bfc1d04ef476f3960bb09879d5a4065fed0
Cindy Yang <sycindy.yang@mail.utoronto.ca>
2020-10-16 14:42:00 -0400

    code and data clean-up

--- R/plotFigure2.R
+++ R/plotFigure2.R
@@ -1,65 +1,46 @@
 ###########
 # plotFigure2.R
 # Created by: Cindy Yang
-# Main R script to create Figure 2 
+# Updated: Oct 16, 2020
+# This script allows the user to to create Figure 2 
+# This script requires the plotSwimmer.R function and Data.Figure2.RData 
+
 ###########
 #####
 # SETUP
 #####
-load("~./data/ASCO_SwimmerPlot_ctDNA_combined_April242019.RData")
-source("~./R/plotSwimmer.R")
-survival.df <- "~./data/data_allpts_updated_20191026_for_cindy.csv"
-
-# Process survival data
-survival.df <- read.csv(survival.df, header = T, stringsAsFactors = F)
-pts.selected <- data.frame(
-  PATIENT_ID = survival.df$INS_No,
-  ctDNAChangeLog = survival.df$change,
-  row.names = survival.df$INS_No
-)
+source("./R/plotSwimmer.R")
+load ("./data/Data.Figure2.RData")
 
 #######
 # PLOT MAIN
 ########
 # 1. Plot swimmer plot on right hand side
-# get patients with change
-
-pdf(height = 12, width = 10, 
-    file = "~./output/Manuscript_Fig_Swimmer_with_change_Oct.pdf")
-
+pdf(height = 12, width = 10, file = "./output/Figure2.pdf")
 split.screen (
-  matrix (
-    c(c(0.250,1,0,1),
-      c(0,0.250,0,1)
-    ),
-    byrow = T, ncol = 4
-  )
+  matrix (c(c(0.12,1,0,1),
+            c(0,0.12,0,1)),
+    byrow = T, ncol = 4)
 );
 
 # 1) Swimmer plot in order based on % change in ctDNA
 screen(1);
-pts.selected <- pts.selected[order(pts.selected$ctDNAChangeLog, decreasing = T),]
-pts.selected <- pts.selected[!is.na(pts.selected$ctDNAChangeLog),]
-plot.dat.netera3 <- plotSwimmer(pt.select = c(row.names(pts.selected),"INS-D-012"),decision = TRUE, reprocess.data = FALSE, plot.legend = FALSE)
+# get patients with change
+pts.selected <- pts.selected[order(pts.selected$ctDNAChangeLog, decreasing = T),];
+pts.selected <- pts.selected[!is.na(pts.selected$ctDNAChangeLog),]; # move the patient with NA to the top of the plot order
+plot.dat.netera3 <- plotSwimmer(pt.select = c(row.names(pts.selected),"INS-D-012"),decision = TRUE,plot.legend = TRUE);
 
 # 2) left hand side % change in ctDNA 
 screen(2);
-#par(mar =c(7.2,1,4.1,0.1));
-par(mar = c(6.15 ,1, 5.2, 0))
-pts.selected$ctDNAChangeLog[pts.selected$ctDNAChangeLog==Inf] <- 45
-log10change <- log10(abs(pts.selected[match(ordered.pts,pts.selected$PATIENT_ID),"ctDNAChangeLog"])*100)
-log10change <- ifelse (pts.selected[match(ordered.pts,pts.selected$PATIENT_ID),"ctDNAChangeLog"]<0,
-                       yes = -1*log10change, 
-                       no = log10change)
-log10change <- log10(abs(pts.selected[,"ctDNAChangeLog"])*100)
-log10change <- ifelse (pts.selected[,"ctDNAChangeLog"]<0,
-                       yes = -1*log10change, 
-                       no = log10change)
+par(mar = c(6.15 ,1, 5.2, 0));
+pts.selected$ctDNAChangeLog[pts.selected$ctDNAChangeLog==Inf] <- 45; # capping maximum at 45
+log10change <- log10(abs(pts.selected[,"ctDNAChangeLog"])*100);
+log10change <- ifelse (pts.selected[,"ctDNAChangeLog"]<0, yes = -1*log10change, no = log10change);
 
 plot(
   x = c(-1*log10change,NA),
   y = plot.dat.netera3$mp,
-  col = adjustcolor(factor(pts.selected$ctDNAChangeLog>0,levels = c(TRUE, FALSE), labels = c("red", "blue")), alpha.f = 1),
+  col = adjustcolor(factor(pts.selected$ctDNAChangeLog>0,levels = c(TRUE, FALSE), labels = c("blue", "red")), alpha.f = 1),
   pch = 16,
   cex = 1,
   xlim = c(-5,2.5),
@@ -72,16 +53,16 @@ plot(
 
 axis(1, at=c(-4:2), labels=c(10000, 1000, 100, 10 ,0, -10,-100),lwd = 1, lwd.tick=1, xaxs="i", cex.axis = 0.5);
 # Add title and axes
-title(xlab = "Percent change mean ctDNA detected, %", main = "ctDNA Change at cycle 3", cex.lab = 0.7, line = 2.50, cex.main = 0.7);
+title(xlab = "Percent change mean\nctDNA detected, %", main = "ctDNA Change\nat cycle 3", cex.lab = 0.60, line = 2.50, cex.main = 0.7);
 
 # Add in ND notation for two samples with no ctDNA detected at baseline
 text(x = 0, y = max(plot.dat.netera3$mp), labels = "ND", cex = 0.70);
 legend("bottomright",
        title = "Change in %\nctDNA mean",
        legend = c("Increase", "Decrease"),
-       fill = c("red", "blue"),
+       fill = c("blue", "red"),
        bty = "n",
-       cex = 0.70);
+       cex = 0.60);
 
 # 3. Close all screens
 close.screen(all = TRUE);
--- R/plotSwimmer.R
+++ R/plotSwimmer.R
@@ -1,79 +1,44 @@
 ###########
 # plotSwimmer.R
 # Created by: Cindy Yang
-# Function to create swimmer plot 
+# Updated: Oct 16, 2020
+# This is a function to create Figure 2 swimmer plot in for Bratman, Yang et al. 2020 Nature Cancer
+#
+# Parameters
+# -----------
+# plot.dat : dataframe, required 
+#      data-frame of plotting data loaded into environment 
+#      patientIDs as rowname 
+# pt.select : string, optional 
+#      string of patientIDs in final plotting order
+#      plotting will be in order of plot.dat if NA
+# main.lab: string, optional, default = ""
+#      string of plot title
+# border: boolean, default = FALSE
+#      boolean control whether border is drawn around legend
+# plot.legend: boolean, default = TRUE
+#      boolean control whether legend is drawn
+# decision: boolean, default = FALSE (depreciated)
+#
+# Returns
+# ------------
+# list: parameters to to regenerate plot
+#    list(plot.dat, mp, ctDNAsummary,n.bar, bar.gap, bar.width, y.max, y.lab)
 ###########
 
-plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariates = list(),
-                         border = FALSE, barcol = FALSE, stacked = TRUE, reprocess.data = FALSE, plot.legend = TRUE){
-  library(plyr)
-  # if stacked == TRUE, then color bars blue for time on treatment, orange for progression free time off trial
-  if (stacked){
-    library(RColorBrewer)
-    barcol <- c(brewer.pal(12, "Paired")[c(11,5,6,1)], "grey")
-    barcol <- c("#FDD901",brewer.pal(12, "Paired")[c(5,6,1)], "grey")
-    
-  }
+plotSwimmer <- function (pt.select, 
+						 main.lab = "", 
+						 decision = FALSE,
+                         border = FALSE, 
+                         plot.legend = TRUE){
+  # 0. Set bar colors
+  library(RColorBrewer)
+  barcol <- c(brewer.pal(12, "Paired")[c(11,5,6,1)], "grey")
+  barcol <- c("#FDD901",brewer.pal(12, "Paired")[c(5,6,1)], "grey")
   
-  # prepare bar information
-  if(reprocess.data) {
-    tm.all.tps.df$time.interval <- unlist(lapply(unique(tm.all.tps.df$PID), 
-                                                 function (x) { sub.dat = tm.all.tps.df[tm.all.tps.df$PID == x,]
-                                                 return (difftime(time1 = as.Date(sub.dat$Date),
-                                                                  time2 = as.Date(sub.dat[1,"Date"]), 
-                                                                  units = "weeks"))}))
-    # get overall response string
-    # merge consecutive scans with the same response
-    timeline <- do.call ("rbind", lapply(unique(as.character(tm.all.tps.df$PID)), 
-                                         function (x) { sub.dat = tm.all.tps.df[tm.all.tps.df$PID == x,]
-                                         vals = rle(as.vector(sub.dat[,"Overall.Response"]))
-                                         interval.start = cumsum(c(1,vals$length[-length(vals$length)]))
-                                         interval.end = interval.start+(vals$length)
-                                         if (max(interval.end)>nrow(sub.dat)) { interval.end[which.max(interval.end)] = nrow(sub.dat)}
-                                         if (length(vals$values)>1){
-                                           interval.length = 
-                                             c(sub.dat[interval.end, "time.interval"][-1], plot.dat[x,"Max.time"]) - 
-                                             sub.dat[interval.start, "time.interval"]
-                                         }else {
-                                           interval.length = plot.dat[x,"Max.time"]
-                                         }
-                                         return(
-                                           data.frame(
-                                             PID = rep (x, length(vals$values)),
-                                             OR = vals$values,
-                                             interval.length = interval.length, 
-                                             interval.start = sub.dat[interval.start, "time.interval"],
-                                             interval.end = sub.dat[interval.start,"time.interval"] + interval.length,
-                                             Max.time = rep(plot.dat[x,"Max.time"], length(vals$values))
-                                           ))}))
-    
-    # Y position according to increasing order so the highest OS pt will be on the top of the plot
-    
-    plot.dat <- plot.dat[unique(as.character(timeline$PID)),]
-    plot.dat <- plot.dat[order(plot.dat[,"Max.time"], decreasing = FALSE),]
-    timeline$colors <- as.character(factor(as.character(timeline$OR), 
-                                           levels = c("SD", "PR", "CR", "PD", "NE"), labels = barcol))
-    
-    # process ctDNA data for plotting
-    plot.dat$WEEKS_SINCE_DIAGNOSIS = difftime(plot.dat$DATE_OF_FIRST_DOSE,
-                                              plot.dat$DATE_OF_DIAGNOSIS, units = "weeks")
-    ctDNA.plot$START_DATE_weeks <- as.double(ctDNA.plot$START_DATE)/7
-    ctDNA.plot$START_DATE_weeks <- ctDNA.plot$START_DATE_weeks - 
-      as.double (plot.dat[as.character(ctDNA.plot$PATIENT_ID),
-                          "WEEKS_SINCE_DIAGNOSIS"])
-    ctDNA.plot$Y.pos <- mp[as.numeric(factor(as.character(ctDNA.plot$PATIENT_ID),
-                                             levels = row.names(plot.dat)[order(plot.dat[,"Max.time"], decreasing = FALSE)]))]
-    max.length = ddply (ctDNA.plot, ~PATIENT_ID, summarize, max.val = max(START_DATE_weeks, na.rm = TRUE))
-    max.length$Y.pos= mp[as.numeric(factor(as.character(max.length$PATIENT_ID), 
-                                           levels = row.names(plot.dat)[order(plot.dat[,"Max.time"], decreasing = FALSE)]))]
-    
-  }else{
-    load ("/Volumes/Samwise/projects/INSPIRE/Review/ctDNA/NeteraMeeting_07052019/ESMO_siwmmer_v2_06082019.Rdata")
-    timeline$colors[timeline$colors=="#FFFF99"] <- "#FDD901"
-  }
-  
-  # subset patients if given, if not, show entire dataset
+  # 1. Subset patients for plotting
   if (!is.na(pt.select)){
+  	library(plyr)
     plot.dat <- plot.dat[match(pt.select,row.names(plot.dat)),]
     mp <- barplot (plot.dat[,"Max.time"], plot= FALSE)[,1]
     timeline <- timeline[as.character(timeline$PID) %in% pt.select,]
@@ -88,7 +53,7 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
                                            levels = row.names(plot.dat)))]
   }
   
-  # plotting start here
+  # 2. Plotting start here
   # Plot axis labels
   x.lab <- "Time since initiation of therapy, wks"
   y.lab <- "Subjects recieved study drug, pts"
@@ -96,15 +61,14 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
   print(x.lim)
   opar <- par(lwd = 0.3)
   
-  # calculate variables that are re-used by multiple functions
+  # 2.1 Calculate variables that are re-used by multiple functions
   n.bar <- nrow(plot.dat)
   bar.gap <- 0.2
   bar.width <- 1
   y.max <- n.bar*bar.width + (1+n.bar)*bar.gap
   
-  #plot each bar
+  # 2.2 Setup empty plot area
   par(mar = c(6.15 ,5, 5.2, 2.1))
-
   plot(NA, 
        ylim = c(0,y.max), 
        xlim = x.lim, 
@@ -113,13 +77,15 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
        yaxs="i",
        xaxt = "n", 
        yaxt = "n")
-  
+       
+  # 2.3 Plot each line segment
   segments(x0 = timeline$interval.start, 
            x1 = timeline$interval.end, 
            y0 = mp[timeline$Y.pos],
            col = timeline$colors,
            lwd = 5)
-  # plot arrow for those continuing treatment
+ 
+  # 2.4 Plot location of end of trial
   points (
     x = plot.dat[plot.dat[,"Still on treatment (y or n)"]=="Y","Max.time"]+1.5,
     y = mp[plot.dat[,"Still on treatment (y or n)"]=="Y"],
@@ -127,29 +93,37 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
     cex = 1,
     col = "black")
   
-  # plot symbol for those off trial not due to progression
-  if(decision){
-    #reasons.keep = c("Death", "Intercurrent Illness", "Investigator's Decision",
-    #                 "Toxicity", "Withdrew consent", "Withdrew Consent")
-    reasons.keep <- "Death"
-    points (
-      x = plot.dat[plot.dat[,"REASON_OFF_TRIAL"] %in% reasons.keep,"Weeks.on.treatment"],
-      y = mp[plot.dat[,"REASON_OFF_TRIAL"] %in% reasons.keep],
-      pch = 4,
-      lwd = 2,
-      cex = 0.70,
-      col = "black")
-  }
+  # 2.5 Plot arrow for those continuing treatment
+  points (
+    x = plot.dat[plot.dat[,"Still on treatment (y or n)"]=="N","Weeks.on.treatment"],
+    y = mp[plot.dat[,"Still on treatment (y or n)"]=="N"],
+    pch = 23,
+    cex = 0.9,
+    lwd = 1,
+    col = "black")
   
-  # plot deaths
+  ## 2.6 Plot symbol for those off trial not due to progression
+  #if(decision){
+  #  reasons.keep <- "Death"
+  #  points (
+  #    x = plot.dat[plot.dat[,"REASON_OFF_TRIAL"] %in% reasons.keep,"Weeks.on.treatment"],
+  #    y = mp[plot.dat[,"REASON_OFF_TRIAL"] %in% reasons.keep],
+  #    pch = 4,
+  #    lwd = 2,
+  #    cex = 0.70,
+  #    col = "black")
+  #}
+  
+  # 2.7 Plot deaths
   points (
     x = plot.dat[as.character(plot.dat[,"OS_STATUS"]) %in% "DECEASED","Max.time"],
     y = mp[as.character(plot.dat[,"OS_STATUS"]) %in% "DECEASED"],
     pch = 4,
     lwd = 2,
     cex = 0.70,
-    col = "black")                                                    
-  
+    col = "black") 
+                                                       
+  # 2.8 ctDNA detection status
   points(
     x = ifelse(ctDNA.plot$START_DATE_weeks<0, 0, ctDNA.plot$START_DATE_weeks),
     y = ctDNA.plot$Y.pos,
@@ -159,11 +133,13 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
     cex = 0.70
   )
   
+  # 2.9 Add Plot title, axes labels and frame
   title(xlab = x.lab, main = "RECIST 1.1 Clincal Response", cex.lab = 0.7, line = 2.50, cex.main = 0.7)
   axis (side = 1, lwd = 1, labels = T, lwd.tick=1, xaxs="i", cex.axis = 0.7)
   axis (side = 2, lwd = 1, labels = row.names(plot.dat), at = mp, lwd.tick=1, xaxs="i", cex.axis = 0.5, las = 2)
   box(lwd = 0.75)
   
+  # 3. Add Figure legend
   swimmer.legend = list(
     labels = c("Stable disease",
                "Partial response",
@@ -171,15 +147,15 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
                "Disease progression",
                "No response data",
                "Continued response",
-               "Death"),
-    ltys = c(1,1,1,1,1, NA, NA),
-    lwds = c(2.5,2.5,2.5,2.5,2.5,NA,1.5),
-    pchs = c(NA,NA,NA,NA,NA,-8594,4),
-    pt.cexs = c(NA,NA,NA,NA,NA,1.5,1.3),
-    pt.cols = c(barcol,"black","black")
+               "Death", 
+               "End of trial"),
+    ltys = c(1,1,1,1,1, NA, NA, NA),
+    lwds = c(2.5,2.5,2.5,2.5,2.5,NA,1.5, 1.2),
+    pchs = c(NA,NA,NA,NA,NA,-8594,4, 23),
+    pt.cexs = c(NA,NA,NA,NA,NA,1.5,1.3, 1),
+    pt.cols = c(barcol,"black","black", "black")
   );
   
-  #plot legend
   coord <- legend("bottomright",
                   title = "RECIST 1.1",
                   legend = swimmer.legend$labels,
@@ -213,8 +189,9 @@ plotSwimmer <- function (pt.select=NA ,main.lab = "", decision = FALSE, covariat
            cex = 0.70)
   }
   
-  return (list(plot.dat = plot.dat, tm.all.tps.df = tm.all.tps.df, mp=mp,covariates=covariates,
+   # 4. Return plot data
+  return (list(plot.dat = plot.dat, mp=mp,
                ctDNAsummary = ctDNAsummary,
-               n.bar =n.bar, bar.gap = bar.gap, bar.width = bar.width, y.max = y.max, y.lab = y.lab,
-               annotcol = as.character(factor(plot.dat[,"INSPIRE COHORT"], levels = names(cancer.cols), labels = cancer.cols))))
+               n.bar =n.bar, bar.gap = bar.gap, bar.width = bar.width, y.max = y.max, y.lab = y.lab)
+               )
 }
